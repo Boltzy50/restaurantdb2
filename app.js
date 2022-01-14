2ed3888f9a27f33005f98feedf432b3abdfb0378
Boltzy50 <90049506+Boltzy50@users.noreply.github.com>
2022-01-13 21:35:28 -0500

    Got foundation working.

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.static("views"));
 // register view engine
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-app.use(express.static(__dirname + "/css"));
+app.use(express.static(__dirname + "/public"));
 
 
 app.get("/", (req, res) => {
