b9eff3216f3fd4015c5c7851f8524576ecb4b6f2
Josh Evans <88986229+Jevans1441@users.noreply.github.com>
2022-01-13 21:35:59 -0500

    setting up css

--- app.js
+++ app.js
@@ -3,13 +3,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use(express.static("views"));
+app.use(express.static("public"));
 // register view engine
 app.set("view engine", "ejs");
 app.set("views", "views");
-
-app.use(express.static(__dirname + "/css"));
-
 
 app.get("/", (req, res) => {
   res.render("pages/index");
